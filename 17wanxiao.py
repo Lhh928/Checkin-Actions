94e683bd93df90c529b9e4f667c7a481476b04b5
Lhh928 <77222647+Lhh928@users.noreply.github.com>
2021-01-10 14:29:59 +0800

    Update 17wanxiao.py
    修改经纬度

--- 17wanxiao.py
+++ 17wanxiao.py
@@ -114,8 +114,8 @@ def receive_check_in(token, custom_id, post_dict):
         "emergencyContactName": post_dict['emergencyContactName'],
         "helpInfo": "",
         "passingCity": "",
-        "longitude": "",  # 请在此处填写需要打卡位置的longitude
-        "latitude": "",  # 请在此处填写需要打卡位置的latitude
+        "longitude": "121.459877",  # 请在此处填写需要打卡位置的longitude
+        "latitude": "30.93626",  # 请在此处填写需要打卡位置的latitude
         "token": token,
     }
     headers = {
